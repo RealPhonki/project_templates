420e504965c01fefc45fadc80dc69bcd0c8412e8
Phonki <138313415+RealPhonki@users.noreply.github.com>
2023-11-09 11:25:38 -0500

    added slash commands

--- discord_bots/cogs/ping.py
+++ discord_bots/cogs/ping.py
@@ -1,23 +1,27 @@
-import discord
+# external imports
+from discord import Embed, app_commands, Interaction
 from discord.ext import commands
 
 class Ping(commands.Cog):
-    def __init__(self, bot: commands.Bot):
+    def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
     
-    @commands.command()
-    async def ping(self, ctx: commands.Context) -> None:
-        # get bot latency from discord api
-        bot_latency = round(self.client.latency * 1000)
+    @app_commands.command(name="ping")
+    async def ping(self, interaction: Interaction) -> None:
+        try:
+            # get bot latency from discord api
+            bot_latency = round(self.bot.latency * 1000)
 
-        # create embed instance
-        embed_message = discord.Embed(title = f"pong : {bot_latency} ms")
-        embed_message.set_author( # set author of embed to the user who requested the command
-            name = f'Requested by {ctx.author.name}',
-            icon_url = ctx.author.avatar
-        )
+            # create embed instance
+            embed_message = Embed(title = f"pong : {bot_latency} ms")
+            embed_message.set_author( # set author of embed to the user who requested the command
+                name = f'Requested by {interaction.user.name}',
+                icon_url = interaction.user.avatar
+            )
 
-        await ctx.send(embed = embed_message)
+            await interaction.response.send_message(embed = embed_message)
+        except Exception as e:
+            print(f"{type(e).__name__}: {e}")
 
 # add cog extension to "client" (the bot)
 # NOTE: THIS CODE RUNS FROM THE DIRECTORY THAT "main.py" IS IN
--- discord_bots/main.py
+++ discord_bots/main.py
@@ -1,41 +1,47 @@
-# so many fuking imports
-import discord
-from discord.ext import commands
-from discord.ext import tasks
+# external imports
+from discord.ext import commands, tasks
+from discord import Intents, Game, __version__
+from asyncio import run
+
+# builtin imports
+from platform import python_version, system, release
 from itertools import cycle
-import platform
 import json
-import sys
 import os
-import asyncio
+
+# local imports
 from bot_logger import logger
 
-# load json file
-if not os.path.isfile(f"{os.path.realpath(os.path.dirname(__file__))}/config.json"):
-    sys.exit("'config.json' not found! Please add it and try again.")
-else:
-    with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
-        config = json.load(file)
+# load config
+with open("config.json") as file:
+    config = json.load(file)
 
-# create instance of the bot
-bot = commands.Bot(command_prefix=commands.when_mentioned_or(config["prefix"]), intents=discord.Intents.all())
-bot.logger = logger()
+# create bot
+client = commands.Bot(command_prefix=commands.when_mentioned_or(config["prefix"]), intents=Intents.all())
+client.logger = logger()
 
-# change the bot status every 5 seconds
-@tasks.loop(seconds=5)
+@tasks.loop(seconds=60)
 async def change_status() -> None:
-    bot_status = cycle(["status 1", "status 2"])
-    await bot.change_presence(activity=discord.Game(next(bot_status)))
+    bot_status = cycle(["Bullet", "Rapid", "Classic", "Atomic", "Duck", "Fog of war"])
+    await client.change_presence(activity=Game(next(bot_status)))
 
-@bot.event
+@client.command(name="sync") 
+async def sync(ctx: commands.Context):
+    synced = await client.tree.sync()
+    print(f"Synced {len(synced)} command(s).")
+    await ctx.send(f"Synced {len(synced)} command(s).")
+
+@client.event
 async def on_ready() -> None:
-    # make this code look super professional and sexy by printing random versions for no reason
-    bot.logger.info(f"Logged in as {client.user.name}")
-    bot.logger.info(f"discord.py API version: {discord.__version__}")
-    bot.logger.info(f"Python version: {platform.python_version()}")
-    bot.logger.info(f"Running on: {platform.system()} {platform.release()} ({os.name})")
-    bot.logger.info("-------------------")
+    # initialize
     change_status.start()
+
+    # debug
+    client.logger.info(f"Logged in as {client.user.name}")
+    client.logger.info(f"discord.py API version: {__version__}")
+    client.logger.info(f"Python version: {python_version()}")
+    client.logger.info(f"Running on: {system()} {release()} ({os.name})")
+    client.logger.info("-------------------")
 
 async def load() -> None:
     # for every python file in the cogs folder
@@ -44,16 +50,16 @@ async def load() -> None:
             extension = filename[:-3]
             try:
                 # load the python extension and print a cool message
-                await bot.load_extension(f"cogs.{extension}")
-                bot.logger.info(f"Loaded extension '{extension}'")
+                await client.load_extension(f"cogs.{extension}")
+                client.logger.info(f"Loaded extension '{extension}'")
             except Exception as e:
                 # print the exception if there is one
                 exception = f"{type(e).__name__}: {e}"
-                bot.logger.error(f"Failed to load extension {extension}\n{exception}")
+                client.logger.error(f"Failed to load extension {extension}\n{exception}")
 
 async def main() -> None:
-    async with bot:
+    async with client:
         await load()
-        await bot.start(config['token'])
+        await client.start(config["token"])
 
-asyncio.run(main())
+run(main())
