ff37c83f247502261de388ac6e7759cdfb87f15c
Phonki <138313415+RealPhonki@users.noreply.github.com>
2023-11-09 20:07:25 -0500

    modified discord bot template

--- discord_bots/cogs/ping.py
+++ discord_bots/cogs/ping.py
@@ -6,24 +6,26 @@ class Ping(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
     
-    @app_commands.command(name="ping")
+    @app_commands.command(name="ping", description="Check if the bot is alive!")
     async def ping(self, interaction: Interaction) -> None:
+        """
+        Checks the latency of the bot.
+        """
         try:
-            # get bot latency from discord api
             bot_latency = round(self.bot.latency * 1000)
 
             # create embed instance
             embed_message = Embed(title = f"pong : {bot_latency} ms")
-            embed_message.set_author( # set author of embed to the user who requested the command
+            embed_message.set_author(
                 name = f'Requested by {interaction.user.name}',
                 icon_url = interaction.user.avatar
             )
 
             await interaction.response.send_message(embed = embed_message)
+            
         except Exception as e:
             print(f"{type(e).__name__}: {e}")
 
-# add cog extension to "client" (the bot)
-# NOTE: THIS CODE RUNS FROM THE DIRECTORY THAT "main.py" IS IN
+# add the extension to the bot
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(Ping(bot))
--- discord_bots/main.py
+++ discord_bots/main.py
@@ -1,6 +1,6 @@
 # external imports
 from discord.ext import commands, tasks
-from discord import Intents, Game, __version__
+from discord import Intents, Embed, Game, __version__
 from asyncio import run
 
 # builtin imports
@@ -12,54 +12,101 @@ import os
 # local imports
 from bot_logger import logger
 
-# load config
-with open("config.json") as file:
-    config = json.load(file)
+class DiscordBot(commands.Bot):
+    def __init__(self) -> None:
+        """
+        Creates some custom attributes before calling the super initializer.
+        """
+        self.config = self.load_config()
+        self.logger = logger()
+        self.add_commands()
 
-# create bot
-client = commands.Bot(command_prefix=commands.when_mentioned_or(config["prefix"]), intents=Intents.all())
-client.logger = logger()
+        super().__init__(self.config["prefix"], intents=Intents.all())
+    
+    def load_config(self) -> dict:
+        """
+        Loads config.json as a json file and returns the contents as a dictionary.
+        """
+        try:
+            with open("config.json") as file:
+                return json.load(file)
+            
+        except Exception as error:
+            raise FileNotFoundError(f"{type(error).name}: {error}")
 
-@tasks.loop(seconds=60)
-async def change_status() -> None:
-    bot_status = cycle(["Bullet", "Rapid", "Classic", "Atomic", "Duck", "Fog of war"])
-    await client.change_presence(activity=Game(next(bot_status)))
+    async def on_ready(self) -> None:
+        """
+        Debug information to confirm that the bot has connected.
+        """
+        self.logger.info(f"Logged in as {self.user.name}")
+        self.logger.info(f"discord.py API version: {__version__}")
+        self.logger.info(f"Python version: {python_version()}")
+        self.logger.info(f"Running on: {system()} {release()} ({os.name})")
+        self.logger.info("-------------------")
 
-@client.command(name="sync") 
-async def sync(ctx: commands.Context):
-    synced = await client.tree.sync()
-    print(f"Synced {len(synced)} command(s).")
-    await ctx.send(f"Synced {len(synced)} command(s).")
+        self.change_status.start()
 
-@client.event
-async def on_ready() -> None:
-    # initialize
-    change_status.start()
+    async def load(self) -> None:
+        """
+        Loops through every python file in the cogs folder and loads it as a cog extension.
+        """
+        for filename in os.listdir("./cogs"):
+            if not filename.endswith(".py"):
+                continue
 
-    # debug
-    client.logger.info(f"Logged in as {client.user.name}")
-    client.logger.info(f"discord.py API version: {__version__}")
-    client.logger.info(f"Python version: {python_version()}")
-    client.logger.info(f"Running on: {system()} {release()} ({os.name})")
-    client.logger.info("-------------------")
-
-async def load() -> None:
-    # for every python file in the cogs folder
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py"):
-            extension = filename[:-3]
+            extension = filename[:-3] # remove the '.py' from the name
             try:
-                # load the python extension and print a cool message
-                await client.load_extension(f"cogs.{extension}")
-                client.logger.info(f"Loaded extension '{extension}'")
-            except Exception as e:
-                # print the exception if there is one
-                exception = f"{type(e).__name__}: {e}"
-                client.logger.error(f"Failed to load extension {extension}\n{exception}")
+                await self.load_extension(f"cogs.{extension}")
+                self.logger.info(f"Loaded extension '{extension}'")
 
-async def main() -> None:
-    async with client:
-        await load()
-        await client.start(config["token"])
+            except Exception as error:
+                exception = f"{type(error).__name__}: {error}"
+                self.logger.error(f"Failed to load extension {extension}\n{exception}")
 
-run(main())
+    @tasks.loop(seconds=60)
+    async def change_status(self) -> None:
+        """
+        Changes the bot status every 60 seconds.
+        """
+        bot_status = cycle(self.config["statuses"])
+        await self.change_presence(activity=Game(next(bot_status)))
+
+    def add_commands(self) -> None:
+        """
+        Adds commands that are required for bot development.
+        """
+        try:
+            @self.command(name="sync") 
+            async def sync(ctx: commands.Context):
+                print("test")
+                try:
+                    synced = await self.tree.sync()
+
+                    # create embed instance
+                    embed_message = Embed(title = f"Synced {len(synced)} command(s)")
+                    embed_message.set_author( # set author of embed to the user who requested the command
+                        name = f'Requested by {ctx.author.name}',
+                        icon_url = ctx.author.avatar
+                    )
+
+                    await ctx.send(embed=embed_message)
+
+                except Exception as e:
+                    await ctx.send(f"{type(e).__name__}: {e}")
+            
+            self.logger.info(f"Loaded command 'sync'")
+
+        except Exception as error:
+            exception = f"{type(error).__name__}: {error}"
+            self.logger.error(f"Failed to load command 'sync'\n{exception}")
+
+    async def main(self) -> None:
+        """
+        Loads the bot and logs into the discord server.
+        """
+        await self.load()
+        await self.start(self.config["token"])
+
+if __name__ == '__main__':
+    discord_bot = DiscordBot()
+    run(discord_bot.main())
